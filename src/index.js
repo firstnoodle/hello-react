9f8cfb903bedb8a9e1be0acd7106db71c828624c
sebastian <seb@firstnoodle.io>
2020-06-23 00:05:02 +0200

    Import index.css in index.js and apply tailwind classes. It works!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Button extends React.Component {
     render() {
-        return <div>YO</div>;
+        return <div className="text-teal-800 font-bold text-xl">YOLO</div>;
     }
 }
 
