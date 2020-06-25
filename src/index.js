e8ce531dca6d304ba56284011cc50686336cda73
sebastian <seb@firstnoodle.io>
2020-06-25 13:43:13 +0200

    index.js → imports App.js and mounts to "root"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-class Button extends React.Component {
-    render() {
-        return <div className="text-teal-800 font-bold text-xl">YOLO</div>;
-    }
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const wrapper = document.querySelector('#container');
-wrapper && ReactDOM.render(<Button />, wrapper);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
+
