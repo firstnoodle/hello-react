ab0f572e4743b8b34536ad951d1c6912da915c92
sebastian <seb@firstnoodle.io>
2020-06-25 13:33:34 +0200

    Added dev dependencies file-loader and babel-plugin-transform-class-properties

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,25 @@ module.exports = {
           }
         ]
       },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ]
   },
   plugins: [
